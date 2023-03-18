92db52bf3bc23447f34013ef2ea2fd6b9e906bee
oseji <jiade1233@gmail.com>
2023-03-18 11:09:16 +0100

    began implementation of currentUser comments

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 "use strict";
-console.log("hey");
+
+//ELEMENTS
 const subContainer = document.querySelector(".subContainer");
-console.log(subContainer);
-// let request = new XMLHttpRequest();
+const commentInput = document.querySelector(".commentInput");
+const submit = document.querySelector(".sendComment");
 
-// request.open("get", "data.json", true);
-// request.send();
-// request.addEventListener("load", function (response) {
-//   const data = JSON.parse(this.response);
-//   console.log(data);
-// });
+const userTag = document.querySelector(".userTag");
+console.log(commentInput, submit);
+//
 
+let currentUsername;
+let currentUserProfile;
+
+//CODE
 fetch("data.json")
   .then(function (response) {
     console.log(response);
@@ -18,22 +20,9 @@ fetch("data.json")
   })
   .then(function (data) {
     console.log(data);
-    console.log(`comments`, data.comments);
-    //console.log(`currentUser`, data.currentUser);
-    data.comments.forEach((i) => {
+
+    data.comments.reverse().forEach((i) => {
       console.log(i);
-      // const id = i.id;
-      // const content = i.content;
-      // const createdAt = i.createdAt;
-      // const score = i.score;
-      // const userImg = i.user.image;
-      // const userName = i.user.username;
-      // console.log(id);
-      // console.log(content);
-      // console.log(createdAt);
-      // console.log(score);
-      // console.log(userImg);
-      // console.log(userName);
 
       const html = `<div class="commentContainer">
       <div class="scoreSection">
@@ -45,10 +34,10 @@ fetch("data.json")
         <div class="nameSection">
           <div class="flex gap-2">
             <img
-              src="${i.user.image.png}"
+              src="${i.user.image.png}" class ="profileImg"
             />
             <p class="name">
-            ${i.user.username} <span class="dateLog">${i.createdAt}</span>
+            ${i.user.username}  <span class="dateLog">${i.createdAt}</span>
             </p>
           </div>
 
@@ -60,6 +49,83 @@ fetch("data.json")
       </div>
     </div>
       `;
+
+      i.replies.reverse().forEach(function (r) {
+        //console.log(r);
+
+        const commentReply = `<div class="replyContainer">
+      <div class="scoreSection">
+        <p class="plus scoreBtn">+</p>
+        <p class="score">${r.score}</p>
+        <p class="minus scoreBtn">-</p>
+      </div>
+      <div class="commentBody">
+        <div class="nameSection">
+          <div class="flex gap-2">
+            <img
+              src="${r.user.image.png}"
+              class="profileImg"
+            />
+            <p class="name">
+            ${r.user.username}<span class="dateLog">${r.createdAt}</span>
+            </p>
+          </div>
+          <img src="images/icon-reply.svg" class="reply" />
+        </div>
+        <div class="content">
+          ${r.content}
+        </div>
+      </div>
+    </div>
+      `;
+        //rendering comment replies
+        subContainer.insertAdjacentHTML("afterbegin", commentReply);
+      });
+      //rendering main comments
       subContainer.insertAdjacentHTML("afterbegin", html);
+    });
+
+    //rendering current user and comments from current user
+    currentUsername = data.currentUser.username;
+    currentUserProfile = data.currentUser.image.png;
+    const time = new Date().toDateString();
+    console.log(time);
+
+    submit.addEventListener("click", function () {
+      console.log("clicked");
+      console.log(commentInput.value);
+
+      const renderUserComment = `
+    <div class="commentContainer">
+      <div class="scoreSection">
+        <p class="plus scoreBtn">+</p>
+        <p class="score">${0}</p>
+        <p class="minus scoreBtn">-</p>
+      </div>
+      <div class="commentBody">
+        <div class="nameSection">
+          <div class="flex gap-2">
+            <img
+              src="${currentUserProfile}" class ="profileImg"
+            />
+            <p class="name">
+            ${currentUsername} <span class="userTag">You</span> <span class="dateLog">${time}</span>
+            </p>
+          </div>
+
+          <img src="images/icon-reply.svg" class="reply" />
+        </div>
+        <div class="content">
+        ${commentInput.value}
+        </div>
+      </div>
+    </div>`;
+
+      if (commentInput.value != "") {
+        subContainer.insertAdjacentHTML("beforeend", renderUserComment);
+        commentInput.value = "";
+      } else {
+        alert("Please type something first");
+      }
     });
   });
